7c469c1b602125a1e8689218a6fcec1929d1ad3d
mimi <allthatjazz@outlook.com>
2021-05-03 09:39:47 -0400

    Non-blocking with try_recv

--- examples/thread.rs
+++ examples/thread.rs
@@ -7,24 +7,21 @@ fn main() {
     let (tx1, rx1): (Sender<u16>, Receiver<u16>) = mpsc::channel();
     let (tx2, rx2): (Sender<u16>, Receiver<u16>) = mpsc::channel();
 
-    thread::spawn(move || {
-        loop {
-            rx2.recv().iter().map(|x| x * x).for_each(|x| {
-                println!("calc thread recv {}", x);
-                &tx1.send(x).unwrap();
-            });
-        }
-    });
-
     thread::spawn(move || loop {
-        rx1.recv().iter().for_each(|x| {
-            println!("result thread recv {}", x);
+        rx2.recv().iter().map(|x| x * x).for_each(|x| {
+            println!("calc thread recv {}", x);
+            &tx1.send(x).unwrap();
         });
     });
 
-    for j in 0..5 {
-        println!("x={}", j);
-        tx2.send(j as u16).unwrap();
-        thread::sleep(Duration::from_secs(1));
+    loop {
+        while let Ok(x) = rx1.try_recv() {
+            println!("result recived {}", x);
+        }
+        for j in 0..3 {
+            println!("x={}", j);
+            tx2.send(j as u16).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     }
 }
